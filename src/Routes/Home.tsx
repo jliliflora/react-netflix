a2e9f91841008895a7cc18c6e0bb8e20cc316ee7
jliliflora <syon704@gmail.com>
2024-07-17 00:51:46 +0900

    9.7~9.8 slider

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -2,9 +2,12 @@ import { useQuery } from "react-query";
 import { getMovies, IGetMoviesResult } from "./api";
 import styled from "styled-components";
 import { makeImagePath } from "./utils";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 
 const Wrapper = styled.div`
     background-color: black;
+    overflow-x: hidden;
 `;
 
 const Loader = styled.div`
@@ -35,20 +38,97 @@ const Overview = styled.p`
     width: 50%; 
 `;
 
+const Slider = styled.div`
+    position: relative;
+    top: -100px;
+`;
+
+const Row = styled(motion.div)`
+    display: grid;
+    gap: 10px;
+    grid-template-columns: repeat(6, 1fr);
+    margin-bottom: 10px;
+    position: absolute;
+    width: 100%;
+`;
+
+const Box = styled(motion.div)<{bgPhoto:string}>`
+    background-color: white;
+    background-image: url(${(props) => props.bgPhoto});
+    background-size: cover;
+    background-position: center center;
+    height: 200px;
+    font-size: 64px;
+`;
+
+const rowVariants = {
+    hidden: {
+        x: window.outerWidth,
+    },
+    visible: {
+        x: 0
+    },
+    exit: {
+        x: -window.outerWidth,
+    },
+}
+
+//pagenation
+const offset = 6; // 한페이지에 보여주고 싶은 영화 수
+
+
 function Home() {
     const { data, isLoading } = useQuery<IGetMoviesResult>(
         ["movies", "nowPlaying"], getMovies
     );
-    console.log(data, isLoading);
+    // console.log(data, isLoading);
+    const [index, setIndex] = useState(0);
+    const [leaving, setLeaving] = useState(false);
+    const increaseIndex = () => {
+        if(data) {
+            if (leaving) return;
+            toggleLeaving();
+            const totalMovies = data.results.length - 1;
+            const maxIndex = Math.floor(totalMovies / offset) - 1; //page가 0에서 시작하기 때문에 -1 해주기
+            setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
+        }  
+    };
+    const toggleLeaving = () => setLeaving((prev) => !prev);
+
     return (
         <Wrapper>{isLoading ? (
             <Loader>Loading...</Loader>
         ) : (
             <>
-                <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+                <Banner 
+                    onClick={increaseIndex}
+                    bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
+                >
                     <Title>{data?.results[0].title}</Title>
                     <Overview>{data?.results[0].overview}</Overview>
                 </Banner>
+                <Slider>
+                    <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
+                    <Row 
+                        variants={rowVariants} 
+                        initial="hidden" 
+                        animate="visible" 
+                        exit="exit" 
+                        transition={{type:"tween", duration: 1}}
+                        key={index}
+                    >
+                        {data?.results
+                            .slice(1)
+                            .slice(offset*index, offset*index+offset)
+                            .map((movie) => (
+                                <Box 
+                                    key={movie.id}
+                                    bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
+                                />
+                            ))}
+                    </Row>
+                    </AnimatePresence>
+                </Slider>
             </>
         ) }</Wrapper>
     );
