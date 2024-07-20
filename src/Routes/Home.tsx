a3bdc8946f6f35a58e7453e017954e06d9206db9
jliliflora <syon704@gmail.com>
2024-07-20 19:27:52 +0900

    9.13 modal

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -100,6 +100,32 @@ const BigMovie = styled(motion.div)`
     left: 0;
     right: 0;
     margin: 0 auto;
+    border-radius: 15px;
+    overflow: hidden;
+    background-color: ${(props) => props.theme.black.lighter};
+`;
+
+const BigCover = styled.div`
+    width: 100%;
+    background-size: cover;
+    background-position: center center;
+    height: 400px;
+`;
+
+const BigTitle = styled.h3`
+    color: ${(props) => props.theme.white.lighter};
+    
+    padding: 20px ;
+    font-size: 46px;
+    position: relative;
+    top: -80px;
+`;
+
+const BigOverview = styled.p`
+    padding: 20px;
+    position: relative;
+    top: -80px;
+    color: ${(props) => props.theme.white.lighter};
 `;
 
 const rowVariants = {
@@ -175,6 +201,8 @@ function Home() {
         history.push(`/movies/${movieId}`);
     };
     const onOverlayClick = () => history.push("/");
+    const clickedMovie = bigMovieMatch?.params.movieId && data?.results.find(movie => movie.id === +bigMovieMatch.params.movieId); //+를 붙이는 이유 : string형을 number형으로 바꾸기 위해서
+    console.log(clickedMovie);
 
     return (
         <Wrapper>{isLoading ? (
@@ -232,7 +260,13 @@ function Home() {
                             <BigMovie
                                 style={{top: scrollY.get() + 100}} //모달 위치를 고정해주기 위해서 scrollY값 넣어줌
                                 layoutId={bigMovieMatch.params.movieId}
-                            />
+                            >
+                                {clickedMovie && <>
+                                    <BigCover style={{backgroundImage:`linear-gradient(to top, black, transparent), url(${makeImagePath(clickedMovie.backdrop_path, "w500")})`}} />
+                                    <BigTitle>{clickedMovie.title}</BigTitle>
+                                    <BigOverview>{clickedMovie.overview}</BigOverview>
+                                </>}
+                            </BigMovie>
                         </>
                     
                     ) : null}
